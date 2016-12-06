a9b13a32c95e85d857c2ad472621913e5bb57d26
Naman Agarwal <niffleheim22@gmail.com>
2016-12-05 22:51:04 -0600

    reworded q's, compiled

--- Question.java
+++ Question.java
@@ -1,6 +1,6 @@
 /**
  * The Question class allows us to create questions
- * 
+ *
  * @author Naman/Jack/Evan
  * @version (a version number or a date)
  */
@@ -8,14 +8,14 @@ public class Question
 {
     // instance variables
     private String question, answer;
-    private Array[String] choices = new Array[4];
+    private String[] choices = new String[4];
     private int dollarAmount;
     private boolean pickedYet;
 
     /**
      * Constructor for objects of class Question
      */
-    public Question(String question, Array[] choices, String answer, int dollarAmount)
+    public Question(String question, String[] choices, String answer, int dollarAmount)
     {
        this.question = question;
        this.answer = answer;
